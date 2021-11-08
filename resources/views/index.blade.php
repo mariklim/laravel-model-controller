665345ccde1da9a00dba9c2efa561a057a323ea6
mariklim <maria.klimenko889@gmail.com>
2021-11-08 18:03:33 +0100

    add google fonts

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;700&family=Raleway:wght@400;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Model Controller Films</title>
 </head>
@@ -11,7 +14,8 @@
     <header>
         <div class="container">
             <img src="{{asset('images/laravel-logo.png')}}" alt="logo laravel">
-            <h1>Model Controller Films with Laravel</h1>
+            <h1>Model Controller Films with <span>Laravel</span>
+                </h1>
         </div>
     </header>
     <main>
@@ -20,11 +24,11 @@
                  @foreach ($movies as $movie)
                  <div class="card">
                      <div class="card-img">
-                         <img src="{{$movie['poster']}}" alt="{{$movie['title']}}" srcset="">
+                         <img src="{{$movie['poster']}}" alt="{{$movie['title']}}">
                      </div>
-                     <h2>{{$movie['title']}}</h2>
-                     <h3>Anno: {{$movie['date']}}</h3>
-                     <h3>Voto: {{$movie['vote']}}</h3>
+                     <h3>{{$movie['title']}}</h3>
+                     <h5>Anno: {{$movie['date']}}</h5>
+                     <h5>Voto: {{$movie['vote']}}</h5>
                  </div>   
                  @endforeach
              </div>
