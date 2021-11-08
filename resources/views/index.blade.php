68e3de89e069f1cc42aef9f0866d98fff1f5f139
mariklim <maria.klimenko889@gmail.com>
2021-11-08 17:51:18 +0100

    add style scss

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,20 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Model Controller Films</title>
 </head>
 <body>
-   <section class="container">
-        <div class="cards">
-            @foreach ($movies as $movie)
-            <div class="card">
-                <img src="{{$movie['poster']}}" alt="{{$movie['title']}}" srcset="">
-                <h2>{{$movie['title']}}</h2>
-                <h3>Anno: {{$movie['date']}}</h3>
-                <h3>Voto: {{$movie['vote']}}</h3>
-            </div>   
-            @endforeach
+    <header>
+        <div class="container">
+            <img src="{{asset('images/laravel-logo.png')}}" alt="logo laravel">
+            <h1>Model Controller Films with Laravel</h1>
         </div>
-   </section> 
+    </header>
+    <main>
+        <section class="container">
+             <div class="cards">
+                 @foreach ($movies as $movie)
+                 <div class="card">
+                     <div class="card-img">
+                         <img src="{{$movie['poster']}}" alt="{{$movie['title']}}" srcset="">
+                     </div>
+                     <h2>{{$movie['title']}}</h2>
+                     <h3>Anno: {{$movie['date']}}</h3>
+                     <h3>Voto: {{$movie['vote']}}</h3>
+                 </div>   
+                 @endforeach
+             </div>
+        </section> 
+    </main>
 </body>
 </html>
